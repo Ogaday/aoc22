136468331eaefe1a6f1f811800aaf570723d1613
Ogaday <w.ogaday@gmail.com>
2022-12-01 18:56:56 +0000

    Feat: Initial solutions to day 1.
    Day 1 puzzles were solved in a REPL and the solutions are simply copied
    from the IPython history. I might tidy them up later, perhaps depending
    on where the rest of the advent goes. I could add tests/CLI interface.
    I'm also not 100% sold on the structure. But this is fine for now.

---
+++ puzzles/day_01/part2.py
@@ -0,0 +1,9 @@
+if __name__ == "__main__":
+    with open("inputs/day_01.txt") as f:
+        data = f.read()
+
+    packs = [
+        [int(val) for val in row.split("\n")] for row in data.strip("\n").split("\n\n")
+    ]
+
+    print(sum(sorted([sum(pack) for pack in packs])[-3:]))
