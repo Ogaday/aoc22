2acf95ee45e5863b629f8dcff60c265842a1b00b
Ogaday <w.ogaday@gmail.com>
2022-12-15 18:42:35 +0000

    Feat: Add solutions for day 12 parts 1 & 2
    Much better with bread first search over depth first search. I had to
    look up the implementation, but happy with how it turned out. In part 2
    it's also possible to exit the BFS after finding the first candidate
    start point, as it's guaranteed to be a start point with the shortest
    path to the end. Still very messy.

--- puzzles/day_12/part2.py
+++ puzzles/day_12/part2.py
@@ -1,7 +1,7 @@
 import string
 import sys
 from collections import namedtuple
-from typing import Dict, List, Union
+from typing import Callable, Dict, List, Union
 import numpy as np
 
 
@@ -20,7 +20,7 @@ def next_steps(position: Vector, maze: np.array) -> Vector:
         if (
             (0 <= new.x < maze.shape[0])
             and (0 <= new.y < maze.shape[1])
-            and (maze[new] - maze[position] <= 1)
+            and (maze[new] - maze[position] >= -1)
         ):
             yield new
 
@@ -31,13 +31,11 @@ def get_path_score(position: Vector, paths: Dict[Vector, Vector]):
     return 1 + get_path_score(paths[position], paths)
 
 
-def bfs(start: Vector, target: Vector, maze: np.array) -> Dict:
+def bfs(start: Vector, maze: np.array) -> Dict:
     nodes = [start]
     paths = {start: None}
     while nodes:
         current = nodes.pop(0)
-        if current == target:
-            break
         for step in next_steps(current, maze):
             if step not in paths:
                 paths[step] = current
@@ -52,14 +50,18 @@ if __name__ == "__main__":
     height_map = {letter: i + 1 for i, letter in enumerate(string.ascii_lowercase)}
     new_maze = np.empty((len(maze), len(maze[0])))
     print(new_maze.shape)
+    starts = []
     for i, row in enumerate(maze):
         for j, column in enumerate(row):
             if column == "S":
                 start = Vector(i, j)
+                starts.append(start)
                 maze[i][j] = "a"
             if column == "E":
                 target = Vector(i, j)
                 maze[i][j] = "z"
+            if column == "a":
+                starts.append(Vector(i, j))
             new_maze[i, j] = height_map[maze[i][j]]
-    paths = bfs(start, target, new_maze)
-    print(get_path_score(target, paths))
+    paths = bfs(target, new_maze)
+    print(min(get_path_score(start, paths) for start in starts if start in paths))
